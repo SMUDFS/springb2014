2278edf1d5c43b0f95463d198ba68ca70ddca34a
TrevinLiberty <trevinliberty@gmail.com>
2014-04-08 18:43:47 -0500

    Initial Level commit
    have level with table and monitors. Can zoom in using fire1 button

---
+++ GameMakerTycoon/Assets/RotateToMainCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateToMainCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Camera playerCam = Camera.main;
+
+		transform.LookAt(playerCam.transform.position);
+	}
+}
