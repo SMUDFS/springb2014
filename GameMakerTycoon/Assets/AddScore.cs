a56b5d9ec73e5e79c4eb001a106c1aa903ddce71
hydraskillz <hydraskillz@gmail.com>
2014-04-08 18:56:52 -0500

    stuff and things

---
+++ GameMakerTycoon/Assets/AddScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddScore : MonoBehaviour {
+
+	public GameObject nguiScorePrefab;
+	private int id;
+
+	void CreateScore( string tributeName )
+	{
+		GameObject newScore = NGUITools.AddChild (GameObject.Find ("ScoreGrid"), nguiScorePrefab);
+		GameObject.Find ("ScoreGrid").GetComponent<UIGrid> ().Reposition ();
+		newScore.transform.FindChild ("NameLabel").GetComponent<UILabel> ().text = tributeName + id;
+		newScore.transform.FindChild ("RankLabel").GetComponent<UILabel> ().text = (++id).ToString();
+		Debug.Log ("Made a new score");
+	}
+
+	void OnClick()
+	{
+		CreateScore ("Name");
+	}
+}
---
+++ GameMakerTycoon/Assets/CreateLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateLevel : MonoBehaviour {
+
+	void OnClick()
+	{
+		string sizeName = GameObject.Find ("SizeList").GetComponent<UIPopupList>().selection;
+		string terrainName = GameObject.Find ("TerrainTypeList").GetComponent<UIPopupList>().selection;
+
+		// Going to want to call a function to creat the level
+
+		Debug.Log ("Creating level: size=\"" + sizeName + "\"" + "terrain=\"" + terrainName + "\"" );
+	}
+}
