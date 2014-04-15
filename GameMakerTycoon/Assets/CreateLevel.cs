d4163302a6f8b21e258a152b0f024304b76fc44c
hydraskillz <hydraskillz@gmail.com>
2014-04-15 17:48:39 -0500

    size things work now

--- GameMakerTycoon/Assets/CreateLevel.cs
+++ GameMakerTycoon/Assets/CreateLevel.cs
@@ -1,7 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CreateLevel : MonoBehaviour {
+
+	private SortedList<string, Vector2> mSizeTable = new SortedList<string, Vector2>();
+
+	void Start()
+	{
+		mSizeTable.Add("Turkish Hovel", new Vector2(5,5));
+		mSizeTable.Add("Small", new Vector2(10,10));
+		mSizeTable.Add("Large", new Vector2(15,15));
+		mSizeTable.Add("Massive", new Vector2(20,20));
+		mSizeTable.Add("Kyles Mom", new Vector2(30,30));
+	}
 
 	void OnClick()
 	{
@@ -9,8 +21,8 @@ public class CreateLevel : MonoBehaviour {
 		string terrainName = GameObject.Find ("TerrainTypeList").GetComponent<UIPopupList>().selection;
 
 		// Going to want to call a function to creat the level
-
-		GameObject.Find("Table").GetComponent<HungerGamesMap>().Resize(5,5);
+		Vector2 size = mSizeTable[sizeName];
+		GameObject.Find("Table").GetComponent<HungerGamesMap>().Resize((int)size.x, (int)size.y);
 
 		Debug.Log ("Creating level: size=\"" + sizeName + "\"" + "terrain=\"" + terrainName + "\"" );
 	}
