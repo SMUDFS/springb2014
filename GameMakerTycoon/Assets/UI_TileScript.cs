8e7f76d2a43d45dd3a8ee5d9d7c8c651f18463e2
hydraskillz <hydraskillz@gmail.com>
2014-04-15 19:27:53 -0500

    the tile thing in the ui is cash money

---
+++ GameMakerTycoon/Assets/UI_TileScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_TileScript : MonoBehaviour {
+	
+	private HungerGamesMap mMap;
+	private GameObject mTile;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		mMap = GameObject.Find("Table").GetComponent<HungerGamesMap>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		HandleInput();
+	}
+	
+	private void HandleInput()
+	{
+		if (Input.GetButtonDown("Fire1")) 
+		{
+			mMap.ChangeTile(ref mTile);
+		}
+	}
+
+	public void SetTile(GameObject tile)
+	{
+		mTile = tile;
+	}
+}
