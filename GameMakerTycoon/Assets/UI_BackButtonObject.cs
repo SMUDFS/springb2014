0b0c8c46de882c376798c2e33b69ef937123a9e5
hydraskillz <hydraskillz@gmail.com>
2014-04-15 20:14:16 -0500

    better ui stuff fuck kyle

---
+++ GameMakerTycoon/Assets/UI_BackButtonObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_BackButtonObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick()
+	{
+		GameObject.Find("Table").GetComponent<HungerGamesMap>().mTableActivated = false;
+	}
+}
---
+++ GameMakerTycoon/Assets/UI_CreateLevelScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_CreateLevelScript : MonoBehaviour {
+
+	public GameObject CreatePanel;
+	public GameObject DesignPanel;
+	private bool mWasCreated = false;
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find("First Person Controller").GetComponent<ActivateStuffScript>().mGUIStartFunc = StartGUIEvent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGUIEvent(GameObject accessedObject)
+	{
+		Debug.Log ("Object was used: " + accessedObject.name);
+		if (accessedObject.name == "Table")
+		{
+			if (mWasCreated)
+			{
+				DesignPanel.SetActive(true);
+				DesignPanel.GetComponent<TweenAlpha>().Play(true);
+				DesignPanel.GetComponent<TweenScale>().Play(true);
+			}
+			else
+			{
+				CreatePanel.SetActive(true);
+				mWasCreated = true;
+			}
+		}
+	}
+}
