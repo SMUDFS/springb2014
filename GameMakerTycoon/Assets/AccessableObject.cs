7a6c35ed982c098fa7449959412150ac73b1fb16
TrevinLiberty <trevinliberty@gmail.com>
2014-04-19 13:49:15 -0500

    forgot lol

---
+++ GameMakerTycoon/Assets/AccessableObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccessableObject : MonoBehaviour {
+
+	public bool mIsBeingAccessed = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		UpdateAccessableObject();
+	}
+
+	public void UpdateAccessableObject()
+	{
+		if(Application.isEditor && Input.GetKeyDown(KeyCode.Escape))
+			mIsBeingAccessed = false;
+	}
+}
