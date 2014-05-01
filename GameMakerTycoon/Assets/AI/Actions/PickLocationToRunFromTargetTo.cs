eee6ae9c505e3a0ee95c51009c7d087f2238601d
hulcyp <prestonhulcy@gmail.com>
2014-05-01 18:37:53 -0500

    Added some prefabs for spawning stuff. And fixed issue with missing sword assets

---
+++ GameMakerTycoon/Assets/AI/Actions/PickLocationToRunFromTargetTo.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Core;
+using RAIN.Action;
+
+[RAINAction]
+public class PickLocationToRunFromTargetTo : RAINAction
+{
+    public PickLocationToRunFromTargetTo()
+    {
+        actionName = "PickLocationToRunFromTargetTo";
+    }
+
+    public override void Start(AI ai)
+    {
+        base.Start(ai);
+    }
+
+    public override ActionResult Execute(AI ai)
+    {
+		//if( ai.Motor.IsAtMoveTarget )
+		{
+			//trying to set this variable: runAwayLocation
+			Vector3 runAwayLocation = ai.Body.gameObject.transform.position;
+			GameObject targetTribute = ai.WorkingMemory.GetItem<GameObject>( "varTribute" );
+			if( targetTribute != null )
+			{
+				Vector3 deltaToTarget = ai.Body.gameObject.transform.position - targetTribute.gameObject.transform.position;
+				runAwayLocation += deltaToTarget.normalized * ( ai.Senses.GetSensor( "ears" ) as RAIN.Perception.Sensors.AudioSensor ).Range;
+				runAwayLocation.y = ai.Body.gameObject.transform.position.y;
+				ai.WorkingMemory.SetItem<Vector3>( "runAwayLocation", runAwayLocation );
+			}
+
+		}
+
+        return ActionResult.SUCCESS;
+    }
+
+    public override void Stop(AI ai)
+    {
+        base.Stop(ai);
+    }
+}
---
+++ GameMakerTycoon/Assets/Scripts/Items/ItemSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSpawner : MonoBehaviour {
+
+	public string mItemName;
+	
+	public bool mShouldSpawn = true;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update()
+	{
+		if( mShouldSpawn )
+		{
+			ItemLoader loader = FindObjectOfType<ItemLoader>();
+			if( loader != null )
+			{
+				GameObject item = loader.SpawnItem( mItemName, transform.position );
+				if( item != null )
+				{
+					Debug.Log( "Item Spawned: Name: " + item.GetComponent<Item>().Name );
+				}
+				Debug.Log( "Tried to spawn item" );
+			}
+			mShouldSpawn = false;
+		}
+	}
+}
