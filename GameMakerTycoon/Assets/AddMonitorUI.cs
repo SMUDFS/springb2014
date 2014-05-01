a40ad7ad08f828531d7c4cfe44998153e6398507
hydraskillz <hydraskillz@gmail.com>
2014-05-01 18:41:58 -0500

    Merge branch 'development' of https://github.com/SMUDFS/springb2014 into development
    Conflicts:
    	GameMakerTycoon/Assets/AI/BehaviorTrees/Tribute.asset
    	GameMakerTycoon/Assets/AI/Cache/BTEditorWindowState.asset
    	GameMakerTycoon/Assets/Scenes/Hulcy_TestBed.unity
    	GameMakerTycoon/GameMakerTycoon.userprefs

---
+++ GameMakerTycoon/Assets/AddMonitorUI.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AddMonitorUI : MonoBehaviour {
+
+	public GameObject MonitorUI;
+	private List<GameObject> mBoyMonitors = new List<GameObject>();
+	private List<GameObject> mGirlMonitors = new List<GameObject>();
+
+	public void AddTribute( Tribute t )
+	{
+		// Find an unused monitor with the correct gender
+		GameObject monitor = null;
+		List<GameObject> monitorList = null;
+		int size = 0;
+
+		if ( t.TheGender == Tribute.Gender.MALE )
+			monitorList = mBoyMonitors;
+		else
+			monitorList = mGirlMonitors;
+
+		size = monitorList.Count;
+
+		if ( size == 0 )
+		{
+			Debug.LogWarning("No more monitors to add tribute info to!");
+			return;
+		}
+
+		monitor = monitorList[size-1];
+		monitorList.RemoveAt(size-1);
+
+		// Assign this monitor to the tribute
+		monitor.GetComponent<UI_MonitorText>().SetTribute( t );
+	}
+
+	// Use this for initialization
+	void Start ()
+	{	
+		foreach(GameObject monitor in GameObject.FindGameObjectsWithTag("Monitor"))
+		{
+			GameObject ui = Instantiate(MonitorUI) as GameObject;
+			ui.transform.parent = monitor.transform;
+
+			ui.transform.rotation = monitor.transform.rotation;
+			ui.transform.Rotate(new Vector3(0, -180, 0));
+			ui.transform.position = monitor.transform.position;
+
+			string[] tok = monitor.name.Split(new char[]{'_'});
+			if ( tok[1] == "Boy" )
+			{
+				//Debug.Log(monitor.name + " It's a boy!");
+				mBoyMonitors.Add(monitor);
+			}
+			else
+			{
+				mGirlMonitors.Add(monitor);
+				//Debug.Log(monitor.name + " It's a girl!");
+			}
+
+			//Debug.Log("Spawn MonitorUI");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+}
---
+++ GameMakerTycoon/Assets/UI_MonitorText.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_MonitorText : MonoBehaviour {
+
+	private UILabel mDistrict;
+	private UILabel mName;
+	private UILabel mRank;
+	private UILabel mStatus;
+
+	private Tribute mTribute = null;
+
+	public void SetTribute( Tribute t )
+	{
+		mTribute = t;
+		UpdateStats();
+	}
+
+	public void OnTributeDeath()
+	{
+		mStatus.text = "[ff0000]DEAD";
+	}
+
+	private void UpdateStats()
+	{
+		if ( mTribute == null )
+		{
+			mDistrict.text = "";
+			mName.text = "";
+			mRank.text = "";
+			mStatus.text = "";
+		}
+		else
+		{
+			mDistrict.text = "District " + mTribute.District.ToString();
+			mName.text = mTribute.Name;
+			mRank.text = "0";
+			mStatus.text = "[00ff00]ALIVE";
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		mDistrict = transform.FindChild("DistrictLabel").gameObject.GetComponent<UILabel>();
+		mName     = transform.FindChild("NameLabel").gameObject.GetComponent<UILabel>();
+		mRank     = transform.FindChild("RankLabel").gameObject.GetComponent<UILabel>();
+		mStatus   = transform.FindChild("StatusLabel").gameObject.GetComponent<UILabel>();
+
+		/*mTribute = new Tribute();
+		mTribute.Name = "Bob";
+		mTribute.District = 4;*/
+		UpdateStats();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
